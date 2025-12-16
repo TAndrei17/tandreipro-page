30737c31e1fb2e89b39e5388b76e45f6a2410dc7
Andrei Trunkin <inboxavt@gmail.com>
2025-12-16 22:30:44 +0100

    chore: add page icon to assets

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,11 +1,13 @@
 import languages from '@assets/icons/languages.png';
 import burgerMenu from '@assets/icons/burger-menu.png';
+import pageBrowser from '@assets/icons/page-browser.png';
 import pageLogo from '@assets/page-logo_40px.svg';
 
 const icons = {
 	pageLogo,
 	languages,
 	burgerMenu,
+	pageBrowser,
 };
 
 export default icons;
