6b0379694038e3f63e515822aff324fdd7aeb72a
Andrei Trunkin <inboxavt@gmail.com>
2025-12-26 15:46:14 +0100

    feat: create AboutMeSection DRAFT

---
+++ src/components/aboutMe/AboutMeSection.tsx
@@ -0,0 +1,17 @@
+import type { Personal } from '@constants/aboutMe';
+import icons from '@constants/icons';
+import './AboutMeStyle.css';
+
+const AboutMeSection = ({ data }: { data: Personal }) => {
+	return (
+		<div className="aboutMe-container">
+			<img src={icons.checkPoint} className="checkPoint-icon" />
+			<div className="content-container">
+				<h2 className="aboutMe-title">{data.title}</h2>
+				<p className="aboutMe-description">{data.description}</p>
+			</div>
+		</div>
+	);
+};
+
+export default AboutMeSection;
--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -1,14 +1,31 @@
+import AboutMeSection from '@components/aboutMe/aboutMeSection';
+import createPersonalList from '@constants/aboutMe';
+import { useSiteHeaderHeight } from '@context/SettingsContext';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const AboutMePage = () => {
 	const { t, i18n } = useTranslation();
 	const language = i18n.language;
+	const { siteHeaderHeight } = useSiteHeaderHeight();
+	const personalDataList = createPersonalList(t);
 
 	useEffect(() => {
 		document.title = t('browserTabs.browserTabAboutMe');
 	}, [t, language]);
-	return <></>;
+	return (
+		<div style={{ paddingTop: siteHeaderHeight + 10 }}>
+			<main>
+				<section className={'service-section'}>
+					<article className={'service-article'}>
+						{personalDataList.map((item) => (
+							<AboutMeSection key={item.id} data={item} />
+						))}
+					</article>
+				</section>
+			</main>
+		</div>
+	);
 };
 
 export default AboutMePage;
