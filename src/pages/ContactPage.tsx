4377d38be4882f467d8a8a5daa8e43baa1b1e894
Andrei Trunkin <inboxavt@gmail.com>
2026-01-14 13:20:37 +0100

    feat: add contacts DRAFT

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,14 +1,44 @@
+import { useSiteHeaderHeight } from '@context/SettingsContext';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
+const PHONE = import.meta.env.VITE_PHONE;
+const EMAIL = import.meta.env.VITE_EMAIL;
+const TELEGRAM = import.meta.env.VITE_TELEGRAM;
+const LINKEDIN = import.meta.env.VITE_LINKEDIN;
+
 const ContactPage = () => {
+	const { siteHeaderHeight } = useSiteHeaderHeight();
 	const { t, i18n } = useTranslation();
 	const language = i18n.language;
 
 	useEffect(() => {
 		document.title = t('browserTabs.browserTabContact');
 	}, [t, language]);
-	return <></>;
+	return (
+		<div style={{ paddingTop: siteHeaderHeight + 10 }}>
+			<p>Со мной можно связаться в любое время удобным для вас способом...</p>
+			<p>Мой второй публичный телефон... (надо сделать)</p>
+			<p>
+				Телефон: <a href={`tel:${PHONE.replace(/\s+/g, '')}`}>{PHONE}</a>
+			</p>
+			<p>
+				Email: <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
+			</p>
+			<p>
+				Telegram:{' '}
+				<a href={`https://t.me/${TELEGRAM}`} target="_blank" rel="noopener noreferrer">
+					@{TELEGRAM}
+				</a>
+			</p>
+			<p>
+				LinkedIn:{' '}
+				<a href={LINKEDIN} target="_blank" rel="noopener noreferrer">
+					{LINKEDIN}
+				</a>
+			</p>
+		</div>
+	);
 };
 
 export default ContactPage;
