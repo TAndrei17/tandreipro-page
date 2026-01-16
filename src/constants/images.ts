6353f936c39b2d78b066c005df9a7b606aeb09ac
Andrei Trunkin <inboxavt@gmail.com>
2026-01-16 13:40:22 +0100

    chore: add images to assets

---
+++ src/constants/images.ts
@@ -0,0 +1,13 @@
+import webSite from '@assets/images/web-site.jpg';
+import shop from '@assets/images/shop.jpg';
+import apps from '@assets/images/apps.jpg';
+import code from '@assets/images/code.jpg';
+
+const images = {
+	webSite,
+	shop,
+	apps,
+	code,
+};
+
+export default images;
