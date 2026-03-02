f72af4334883c83d6e2d248c2360615835a2efbb
Andrei Trunkin <inboxavt@gmail.com>
2026-03-02 21:50:50 +0100

    feat: create tags section&components

--- src/components/dashboard/TagsSections.tsx
+++ src/components/dashboard/TagsSections.tsx
@@ -1,13 +1,36 @@
 import { useTranslation } from 'react-i18next';
-import './styles/Sections.css';
+
+import './styles/TagsSection.css';
+import { useAppSelector } from '@store/index';
+import { tagsSelectors } from '@store/tags/selectors';
 
 const TagsSection = () => {
 	const { t: tNav } = useTranslation('translation', { keyPrefix: 'dashboard.nav' });
+	const { t: tTags } = useTranslation('translation', { keyPrefix: 'dashboard.tags' });
+	const tags = useAppSelector(tagsSelectors.selectAll);
 
 	return (
-		<section id={'tags'}>
-			<div className={'dashboard-section-content'}>
-				<h2 className={'dashboard-section-header'}>{tNav('tags')}</h2>
+		<section id="tags">
+			<div className="dashboard-section-content">
+				{/* Header */}
+				<div className="dashboard-section-header-container">
+					<h2 className="dashboard-section-header">{tNav('tags')}</h2>
+
+					<button className="create-tag-button">{`+ ${tTags('createTag')}`}</button>
+				</div>
+
+				{/* Tags list */}
+				<div className="tags-list">
+					{tags.map((tag) => (
+						<div key={tag.id} className="tag-item">
+							<span className="tag-name">{tag.name}</span>
+
+							<button className="delete-tag-button" title={tTags('deleteTag')}>
+								✕
+							</button>
+						</div>
+					))}
+				</div>
 			</div>
 		</section>
 	);
