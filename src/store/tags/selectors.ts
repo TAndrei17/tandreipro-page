d0a8c5d33e346c9e07681f1de045abc9a7f79820
Andrei Trunkin <inboxavt@gmail.com>
2026-03-03 16:03:59 +0100

    feat: create custom selector getTags

--- src/store/tags/selectors.ts
+++ src/store/tags/selectors.ts
@@ -1,5 +1,18 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 import type { RootState } from '../index';
 
 import { tagsAdapter } from './tagsSlice';
 
 export const tagsSelectors = tagsAdapter.getSelectors<RootState>((state) => state.tags);
+
+export const selectTagsByIds = createSelector(
+	[tagsSelectors.selectEntities, (_: RootState, ids?: number[]) => ids],
+	(entities, ids) => {
+		if (!ids || ids.length === 0) {
+			return [];
+		}
+
+		return ids.map((id) => entities[id]).filter(Boolean);
+	}
+);
